30c8d4c401179a6709cf0201a7cda79703ea808c
Derick Swekenyi <41902897+swekenyi@users.noreply.github.com>
2024-03-29 09:12:44 +0300

    HomeScreen.js

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+// screens/HomeScreen.js
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => (
+  <View>
+    <Text>Welcome to HealthLink!</Text>
+    {/* Add more components and functionality as needed */}
+  </View>
+);
+
+export default HomeScreen;
